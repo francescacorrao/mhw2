fac0228fb2226d4db14e5fc45e90ef6e74e82e38
francescacorrao <81303788+francescacorrao@users.noreply.github.com>
2021-04-12 17:53:05 +0200

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,60 @@
+const title = ["Bodyguard","Game Of Thrones", "Grey's Anatomy", "Lost", "Once Upon A Time", "Riverdale","Sense8","Supernatural", "The Big Bang Theory"];
+const image = ['bodyguard.jpg', 'GOT.jpeg', 'GA.jpg', 'lost.jpg', 'OUAT.jpg', 'riverdale.jpg', 'Sense8.jpg', 'spn.jpg', 'tbbt.jpg'];
+const denscription = ["Dopo aver contribuito a sventare un attacco di terroristico un veterano di guerra deve proteggere un esponente politica, promotrice del conflitto che ha combatutato.",
+"Nobili famiglie combattono per il controllo del regno e i loro attriti portano a grandi guerre, tutto questo mentre un antico nemico si risveglia fuori dai confini.",
+"Giovani medici svolgono il loro tirocinio presso il Grace Hospital di Seattle, intrecciando le loro vite professionali e sentimentali",
+"Dopo lo schianto di un aereo in un isola deserta i naufraghi devono adattarsi a convivere e fronteggiare le forze del luogo.",
+"In una città i personaggi delle fiabe vivono una vita normale non ricordando la loro verà identità, solo un bambino conosce la verità e cerca di risveglaire i ricordi degli abitanti.",
+"In una città sconvolta dalla tragica morte di un liceale, un gruppo di giovani cerca di fare chiarezza sulla faccenda mentre affronta la vita di tutti i giorni.",
+"Questa serie racconta di otto sconosciuti che condivisono esperienze grazie a un legame telepatico.",
+"Due fratelli, Sam e Dean Winchester, e la loro lotta conto le forze del male che hanno causato la morte della madre.",
+"Un gruppo di quattro giovani scienzieti e un aspirante attrice loro vicina di casa nella loro vita quotidiana"];
+const generi= ["Thriller", "Fantasy", "Drama", "Science Fiction", "Fantasy", "Mystery", "Suspance", "Action", "Comedy"];
+/*const contenuti =[
+    {
+        titolo = 'Bodyguard',
+        immagine= 'bodyguard.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Game Of Thrones',
+        immagine='GOT.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Grey Anatomy',
+        immagine='GA.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Lost',
+        immagine='lost.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Once Upon A Time',
+        immagine='OUAT.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Riverdale',
+        immagine='riverdale.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Sense8',
+        immagine='Sense8.jpg',
+        descrizione=""
+    },
+    {
+        titolo='Supernatural',
+        immagine='spn.jpg',
+        descrizione=""
+    },
+    {
+        titolo='The Big Bang Theory',
+        immagine='tbbt.jpg',
+        descrizione=""
+    },
+
+];*/
